43dbf9a5207d1e8e1cb0afd3f74316d091e363bf
developerdatascience <harendra091016@gmail.com>
2022-08-02 20:42:25 +0530

    inital files added

---
+++ src/dataloder.py
@@ -0,0 +1,8 @@
+import torch
+import torchvision
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+import math
+
+
+
---
+++ src/eda.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+
+telecom_data = pd.read_csv("inputs/telecom_raw.csv")
+
