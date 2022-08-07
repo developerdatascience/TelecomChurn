7f376ef4d11d3d559c8aa01ca5aa599c0b5beb30
developerdatascience <harendra091016@gmail.com>
2022-08-07 13:41:18 +0530

    Neural Network Added

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -2,6 +2,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
+from sklearn.preprocessing import MinMaxScaler
+
+
 telecom_data = pd.read_csv("inputs/telecom_raw.csv")
 
 print(telecom_data.shape)
@@ -35,3 +38,35 @@ for column in yes_no_columns:
 for col in modified_telecom_data:
     if modified_telecom_data[col].dtypes == 'object':
         print(f'{col}: {modified_telecom_data[col].unique()}')
+
+modified_telecom_data["gender"].replace({"Male":0, "Female":1}, inplace= True)
+
+# Creating new dataframe with encoded categorical variables
+
+df_encoded = pd.get_dummies(data=modified_telecom_data, columns=["InternetService", "Contract", "PaymentMethod"])
+
+def drop_bad_col():
+    for col in df_encoded:
+        if "Unnamed: 0" in col:
+            print("===========Dropping Unnamed: 0 column============")
+            return df_encoded.drop("Unnamed: 0", axis=1, inplace=True)
+        else:
+            print("===========No Unnamed: 0 column found============")
+            return df_encoded
+
+drop_bad_col()
+
+
+cols_to_scale = ["tenure", "MonthlyCharges", "TotalCharges"]
+
+
+scaler = MinMaxScaler()
+
+df_encoded[cols_to_scale] = scaler.fit_transform(df_encoded[cols_to_scale])
+
+
+print("=========Exporting the Transformed data to csv format============")
+df_encoded.to_csv("inputs/Processed_data.csv", index=False)
+print("=========Data Exported to csv format===============")
+
+
